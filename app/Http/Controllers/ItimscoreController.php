b22f72635dca45b75fbf1bd4f2ae60a8d641b6a0
ewqdsacxz <Patric_emmel@outlook.com>
2018-03-29 11:31:04 +0700

    [ADD] itimscore Controller

---
+++ app/Http/Controllers/ItimscoreController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\ItimscoreRepository;
+
+
+class ItimscoreController extends Controller
+{
+    public function __construct() 
+    {
+        $this->itimscore = new ItimscoreRepository;
+    }
+
+    public function getAll()
+    {
+        return $this->itimscore->getAll();
+    }
+
+    public function put(Request $request)
+    {
+        //json_decode
+        $data = $request->input('data');
+        return $this->itimscore->put($data);
+    }
+
+    public function post(Request $request)
+    {
+        //json_decode
+        $data = $request->input('data');
+        return $this->itimscore->put($data);
+    }
+    
+}
